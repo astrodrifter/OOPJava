6352ffed93bfc47cf8b4cb737913aaa2419a6525
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-08-31 13:56:21 +1000

    first hello world program

---
+++ Programming1/src/Tutorial1.java
@@ -0,0 +1,13 @@
+/* this program is to answer questions from tutorial 1 
+ * 
+ * 31/08/17
+ * 
+ * By Dhruva O'Shea 
+ * 
+ */
+
+public class Tutorial1 {
+   public static void main(String[] args) {
+	   System.out.println("Hello World");
+   }
+}
