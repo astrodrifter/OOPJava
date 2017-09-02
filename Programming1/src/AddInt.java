8a2ff0f765dcf43ab7e22f31a0c980fae64600d1
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-09-02 21:57:42 +1000

    java integer adding program

---
+++ Programming1/src/AddInt.java
@@ -0,0 +1,26 @@
+/* This program takes two integers from the keyboard,
+ * adds them and prints the sum to console.
+ * 
+ * Dhruva O'Shea 02/09/17
+ */
+import java.io.*;
+public class AddInt {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		String first, second;
+		int firstNum, secondNum, sum;
+		
+		System.out.print("Input an integer numer: ");
+		first = stdin.readLine();
+		firstNum = Integer.parseInt(first);
+		
+		System.out.print("Input another integer numer: ");
+		second = stdin.readLine();
+		secondNum = Integer.parseInt(second);
+		
+		sum = firstNum+secondNum;
+		System.out.println("The sum is: " + sum);
+	}
+
+}
