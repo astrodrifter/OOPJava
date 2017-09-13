05a6153b92a24c9aa5c0a18d218da3838e7137fd
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-09-13 22:34:50 +1000

    conditional operator

---
+++ Programming1/src/ConditonalOperator.java
@@ -0,0 +1,14 @@
+/*This program is a conditional operator example
+ * 
+ * Dhruva O'Shea 13/09/17
+ */
+
+public class ConditonalOperator {
+
+	public static void main(String[] args) {
+		int x = 5, y = 0, minVal;
+		minVal = (x > y) ? y : x;
+		System.out.println("Min = "+minVal);
+	}
+
+}
