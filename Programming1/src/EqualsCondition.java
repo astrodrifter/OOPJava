203189b0110a35d0fc80d067550176d37cf2e287
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-09-14 17:08:47 +1000

    using string.equal("a string")

---
+++ Programming1/src/EqualsCondition.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+public class EqualsCondition extends ConditonalOperator {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String myString = console.nextLine();
+		
+		if (myString.equals("Suryani") || myString.equals("Sarasvati")){
+			System.out.println("You are my daughter "+myString);
+		} else {
+			System.out.println("You are not my daughter, "+myString);
+		}
+
+	}
+
+}
