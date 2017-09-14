fed95110e2bbe9b40de6738fc3cbdf4a3d7f8a4d
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-09-14 18:58:07 +1000

    Stage one fee calculated

--- Programming1/src/StageOne.java
+++ Programming1/src/StageOne.java
@@ -7,19 +7,32 @@ import java.io.*;
 public class StageOne {
 
 	public static void main(String[] args) throws IOException{
+		
 		//open buffer reader
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		
 		// get customer details
 		String title, name, email, mobile, address;
 		System.out.println("Enter your title Mr, Mrs, Miss, Dr:");
 		title = keyboard.readLine();
-		System.out.println("Enter your name in the format Firstname Lastname:");
+		System.out.println("Enter your name in the format First Last:");
 		name = keyboard.readLine();
 		System.out.println("Enter your email address:");
 		email = keyboard.readLine();
 		System.out.println("Enter your home address:");
 		address = keyboard.readLine();
+		
+		
+		// Get number of toll sectors passed and calculate fee
+		String sectorsString;
+		int sectors;
+		double fee;
+		System.out.println("How many toll sectors did you pass?");
+		sectorsString = keyboard.readLine();
+		sectors = Integer.parseInt(sectorsString);
+		fee = sectors*2.50;
 		System.out.printf("Name        %s   %s\nEmail           %s\nHome address    %s\n",title,name,email,address);
+		System.out.printf("Toll fee is     $%.2f\n",fee);
 	}
 
 }
