f8ed9d8cba8937a83807997b5e65c7dfabbe5104
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-10-27 10:23:43 +1000

    attributes

--- s3622499_assignment2/src/Parcel.java
+++ s3622499_assignment2/src/Parcel.java
@@ -3,5 +3,16 @@ public class Parcel
 {
    // implement the different elements of this Parcel class as described in
    // Stage 1 of the specification
+	
+	//Attributes
+	private String parcelNumber;
+	private String senderName;
+	private String returnAddress;
+	private String recipientName;
+	private String deliveryAddress;
+	private String contactNumber;
+	private String satchelSize;
+	private boolean deliveryComplete;
+	private String trackingHistory;
    
 }
